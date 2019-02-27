d40b33af1b60e4546ce4a4e80ab4bd9b87a20086
jakebacker <jbacker42@gmail.com>
2019-02-27 08:31:37 -0500

    I HATE GIT

--- src/main/java/org/robockets/deepspace/OI.java
+++ src/main/java/org/robockets/deepspace/OI.java
@@ -7,39 +7,150 @@
 
 package org.robockets.deepspace;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import org.robockets.deepspace.cargo.*;
+import org.robockets.deepspace.climber.*;
+import org.robockets.deepspace.hatch.*;
+import org.robockets.deepspace.misccommands.ToggleCompressor;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-  //// CREATING BUTTONS
-  // One type of button is a joystick button which is any button on a
-  //// joystick.
-  // You create one by telling it which joystick it's on and which button
-  // number it is.
-  // Joystick stick = new Joystick(port);
-  // Button button = new JoystickButton(stick, buttonNumber);
+	// Driver
+	public static Joystick joystick = new Joystick(0);
 
-  // There are a few additional built in buttons you can use. Additionally,
-  // by subclassing Button you can create custom triggers and bind those to
-  // commands the same as any other Button.
+	public static JoystickButton aButton = new JoystickButton(joystick, 1);
+	public static JoystickButton bButton = new JoystickButton(joystick, 2);
+	public static JoystickButton xButton = new JoystickButton(joystick, 3);
+	public static JoystickButton yButton = new JoystickButton(joystick, 4);
 
-  //// TRIGGERING COMMANDS WITH BUTTONS
-  // Once you have a button, it's trivial to bind it to a button in one of
-  // three ways:
+	public static JoystickButton leftBumper = new JoystickButton(joystick, 5);
+	public static JoystickButton rightBumper = new JoystickButton(joystick, 6);
 
-  // Start the command when the button is pressed and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenPressed(new ExampleCommand());
+	public static JoystickButton selectButton = new JoystickButton(joystick, 7);
+	public static JoystickButton startButton = new JoystickButton(joystick, 8);
 
-  // Run the command while the button is being held down and interrupt it once
-  // the button is released.
-  // button.whileHeld(new ExampleCommand());
-  public static Joystick joystick = new Joystick (0);
 
-  // Start the command when the button is released and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenReleased(new ExampleCommand());
+	// Operator 1
+	public static Joystick buttonBoard1 = new Joystick(1);
+
+	public static JoystickButton button101 = new JoystickButton(buttonBoard1, 1);
+	public static JoystickButton button102 = new JoystickButton(buttonBoard1, 2); // Cargo Low
+	public static JoystickButton button103 = new JoystickButton(buttonBoard1, 3); // Hatch all out
+	public static JoystickButton button104 = new JoystickButton(buttonBoard1, 4); // Cargo shoot
+	public static JoystickButton button105 = new JoystickButton(buttonBoard1, 5); // Climb
+	public static JoystickButton button106 = new JoystickButton(buttonBoard1, 6); // Cargo Intake
+	public static JoystickButton button107 = new JoystickButton(buttonBoard1, 7);
+	public static JoystickButton button108 = new JoystickButton(buttonBoard1, 8); // Cargo Ship
+	public static JoystickButton button109 = new JoystickButton(buttonBoard1, 9); // Cargo out
+	public static JoystickButton button110 = new JoystickButton(buttonBoard1, 10); // Hatch Start
+	public static JoystickButton button111 = new JoystickButton(buttonBoard1, 11); // Hatch Release
+	public static JoystickButton button112 = new JoystickButton(buttonBoard1, 12); // Hatch pickup
+	public static JoystickButton button113 = new JoystickButton(buttonBoard1, 13); //+
+	public static JoystickButton button114 = new JoystickButton(buttonBoard1, 14);
+	public static JoystickButton button115 = new JoystickButton(buttonBoard1, 15);
+	public static JoystickButton button116 = new JoystickButton(buttonBoard1, 16);
+	public static JoystickButton button117 = new JoystickButton(buttonBoard1, 17);
+	public static JoystickButton button118 = new JoystickButton(buttonBoard1, 18);
+	public static JoystickButton button119 = new JoystickButton(buttonBoard1, 19); // Set climber left
+	public static JoystickButton button120 = new JoystickButton(buttonBoard1, 20); // Set climber right
+	public static JoystickButton button121 = new JoystickButton(buttonBoard1, 21);
+	public static JoystickButton button122 = new JoystickButton(buttonBoard1, 22); // Cargo Ground
+	public static JoystickButton button123 = new JoystickButton(buttonBoard1, 23);
+
+	// Operator 2
+	public static Joystick buttonBoard2 = new Joystick(2);
+
+	public static JoystickButton button201 = new JoystickButton(buttonBoard2, 1);
+	public static JoystickButton button202 = new JoystickButton(buttonBoard2, 2); // Intake Backward
+	public static JoystickButton button203 = new JoystickButton(buttonBoard2, 3); // Intake Forward
+	public static JoystickButton button204 = new JoystickButton(buttonBoard2, 4); // Climbing Wheels Forward
+	public static JoystickButton button205 = new JoystickButton(buttonBoard2, 5); // Climbing Piston Down
+	public static JoystickButton button206 = new JoystickButton(buttonBoard2, 6);
+	public static JoystickButton button207 = new JoystickButton(buttonBoard2, 7); // Inner Pistons Close
+	public static JoystickButton button208 = new JoystickButton(buttonBoard2, 8); // Climbing Arms Down
+	public static JoystickButton button209 = new JoystickButton(buttonBoard2, 9); // Outer Pistons Close
+	public static JoystickButton button210 = new JoystickButton(buttonBoard2, 10); // Climbing Piston Up
+	public static JoystickButton button211 = new JoystickButton(buttonBoard2, 11); // Climbing Arms Up
+	public static JoystickButton button212 = new JoystickButton(buttonBoard2, 12); // Inner Pistons Open
+	public static JoystickButton button213 = new JoystickButton(buttonBoard2, 13); // Outer Pistons Open
+	public static JoystickButton button214 = new JoystickButton(buttonBoard2, 14);
+	public static JoystickButton button215 = new JoystickButton(buttonBoard2, 15);
+	public static JoystickButton button216 = new JoystickButton(buttonBoard2, 16);
+	public static JoystickButton button217 = new JoystickButton(buttonBoard2, 17);
+	public static JoystickButton button218 = new JoystickButton(buttonBoard2, 18); // Cargo Piston Up
+	public static JoystickButton button219 = new JoystickButton(buttonBoard2, 19);
+	public static JoystickButton button220 = new JoystickButton(buttonBoard2, 20); // Cargo Piston Down
+	public static JoystickButton button221 = new JoystickButton(buttonBoard2, 21); // Climbing Wheels Backward
+	public static JoystickButton button222 = new JoystickButton(buttonBoard2, 22); // Arm Backward
+	public static JoystickButton button223 = new JoystickButton(buttonBoard2, 23); // Arm Forward
+
+
+	public OI() {
+		//aButton.whileHeld(new MoveArms());
+		/*bButton.whenPressed(new RetractPistons());
+		xButton.whenPressed(new ExtendPistons());*/
+		//yButton.whenPressed(new ToggleCompressor());
+		aButton.whenPressed(new SetHatch(DoubleSolenoid.Value.kReverse, DoubleSolenoid.Value.kReverse));
+		bButton.whenPressed(new SetHatch(DoubleSolenoid.Value.kForward, DoubleSolenoid.Value.kReverse));
+		xButton.whenPressed(new SetHatch(DoubleSolenoid.Value.kReverse, DoubleSolenoid.Value.kForward));
+		yButton.whenPressed(new SetHatch(DoubleSolenoid.Value.kForward, DoubleSolenoid.Value.kForward));
+		//rightBumper.whenPressed(new ToggleCompressor());
+		//leftBumper.whileHeld(new RunBottomWheelsManual(0.75));
+		//leftBumper.whenPressed(new Climb());
+		//yButton.whenPressed(new ToggleCargoDirection());
+		//xButton.whenPressed(new ExtendPistons());
+		//bButton.whenPressed(new RetractPistons());
+		//selectButton.whileHeld(new RunBottomWheelsManual(0.3));
+		startButton.whenPressed(new ToggleCompressor());
+
+		/*leftBumper.whileHeld(new MoveArms(-1.0));
+		rightBumper.whileHeld(new MoveArms(1.0));*/
+
+		// Hatch
+		button103.whenPressed(new SetHatch(DoubleSolenoid.Value.kForward, DoubleSolenoid.Value.kForward)); // All out
+		button110.whenPressed(new RetractHatchPistons());
+		button111.whenPressed(new EjectHatch());
+		button112.whenPressed(new GrabHatch());
+
+		button212.whenPressed(new SetInnerHatch(DoubleSolenoid.Value.kForward));
+		button207.whenPressed(new SetInnerHatch(DoubleSolenoid.Value.kReverse));
+
+		button213.whenPressed(new SetOuterHatch(DoubleSolenoid.Value.kForward));
+		button209.whenPressed(new SetOuterHatch(DoubleSolenoid.Value.kReverse));
+
+		// Cargo
+		button106.whileHeld(new RunCargoIntake(1));
+		button102.whenPressed(new SetCargoPos(CargoPos.LOW));
+		button109.whileHeld(new RunCargoIntake(-1));
+		button108.whenPressed(new SetCargoPos(CargoPos.CARGO));
+		button122.whenPressed(new SetCargoPos(CargoPos.PICKUP));
+
+		button203.whileHeld(new RunCargoIntake(1));
+		button202.whileHeld(new RunCargoIntake(-1));
+
+		button223.whileHeld(new MoveCargoArm(0.5));
+		button222.whileHeld(new MoveCargoArm(-0.5));
+
+		button218.whenPressed(new SetCargoPiston(DoubleSolenoid.Value.kForward));
+		button220.whenPressed(new SetCargoPiston(DoubleSolenoid.Value.kReverse));
+
+
+		// Climber
+		button105.whileHeld(new Climb());
+
+		button204.whileHeld(new RunBottomWheelsManual(0.3));
+		button221.whileHeld(new RunBottomWheelsManual(-0.3));
+
+		button208.whileHeld(new RunArms(-0.45));
+		button211.whileHeld(new RunArms(0.45));
+
+		button205.whenPressed(new ExtendPistons());
+		button210.whenPressed(new RetractPistons());
+
+	}
 }
--- src/main/java/org/robockets/deepspace/RobotMap.java
+++ src/main/java/org/robockets/deepspace/RobotMap.java
@@ -7,9 +7,11 @@
 
 package org.robockets.deepspace;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
@@ -19,16 +21,8 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
  * floating around.
  */
 public class RobotMap {
-  // For example to map the left and right motors, you could define the
-  // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
 
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
-
+    // Drivetrain
     public static CANSparkMax frontLeftMotorController = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
     public static CANSparkMax backleftMotorController = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
     public static CANSparkMax frontRightMotorController = new CANSparkMax(0, CANSparkMaxLowLevel.MotorType.kBrushless);
@@ -37,4 +31,53 @@ public class RobotMap {
     public static SpeedControllerGroup left = new SpeedControllerGroup(frontLeftMotorController, backleftMotorController);
     public static SpeedControllerGroup right = new SpeedControllerGroup(frontRightMotorController, backRightMotorController);
     public static DifferentialDrive robotDrive = new DifferentialDrive(left, right);
+
+    public static CANEncoder frontLeftEncoder = frontLeftMotorController.getEncoder();
+    public static CANEncoder backLeftEncoder = backleftMotorController.getEncoder();
+    public static CANEncoder frontRightEncoder = frontRightMotorController.getEncoder();
+    public static CANEncoder backRightEncoder = backRightMotorController.getEncoder();
+
+    public static ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+
+    // Climber
+
+    public static CANSparkMax leftClimber = new CANSparkMax(5, CANSparkMaxLowLevel.MotorType.kBrushless);
+    public static CANSparkMax rightClimber = new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+    public static SpeedControllerGroup climberMotors = new SpeedControllerGroup(leftClimber, rightClimber);
+
+    public static CANEncoder leftClimberEncoder = leftClimber.getEncoder();
+    public static CANEncoder rightClimberEncoder = rightClimber.getEncoder();
+
+    public static CANSparkMax climberWheels = new CANSparkMax(6, CANSparkMaxLowLevel.MotorType.kBrushed);
+
+    public static DoubleSolenoid climberSolenoids = new DoubleSolenoid(4, 5);
+
+    public static DigitalInput leftLimitSwitch = new DigitalInput(2);
+    public static DigitalInput rightLimitSwitch = new DigitalInput(3);
+
+    // Hatch
+
+    public static DoubleSolenoid outerPusher = new DoubleSolenoid(0,1);
+    public static DoubleSolenoid middlePusher = new DoubleSolenoid(2, 3);
+
+    // Cargo
+
+    public static CANSparkMax cargoArmMotor = new CANSparkMax(7, CANSparkMaxLowLevel.MotorType.kBrushless);
+    //public static WPI_VictorSPX cargoIntakeMotor = new WPI_VictorSPX(7);
+    public static CANSparkMax cargoIntakeMotor = new CANSparkMax(8, CANSparkMaxLowLevel.MotorType.kBrushed);
+
+    public static CANEncoder cargoEncoder = cargoArmMotor.getEncoder();
+
+    public static DoubleSolenoid cargoSolenoid = new DoubleSolenoid(6, 7);
+
+    public static DigitalInput cargoBreakbeam = new DigitalInput(0);
+
+    // Misc
+    public static Compressor compressor = new Compressor();
+    public static AnalogInput pressure1 = new AnalogInput(0);
+    public static AnalogInput pressure2 = new AnalogInput(1);
+
+
+    public static Spark ledStrip = new Spark(1);
 }
